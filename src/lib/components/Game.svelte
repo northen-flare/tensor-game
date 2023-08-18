82a7a10a8e1d2f338356484a1b0cb006e50b7465
mv.baranov <mv.baranov@tensor.ru>
2023-08-18 18:36:59 +0300

    Minor changes

--- src/lib/components/Game.svelte
+++ src/lib/components/Game.svelte
@@ -1,53 +1,80 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	
+	enum FaseEnum {
+		'BEGIN',
+		'QUESTION',
+		'BID',
+		'END'
+	}
 
-	let canvas: HTMLCanvasElement;
-	onMount(async () => {
-		const Phaser = (await import('phaser')).default;
-		const LoadScene = (await import('$lib/scenes/loadScene')).default;
-		const PlayScene = (await import('$lib/scenes/playScene')).default;
-		const BreakoutScene = (await import('$lib/scenes/breakoutScene')).default;
+	let money: number = 20;
+	let round: number = 0;
+	let fase: FaseEnum = FaseEnum.BEGIN;
 
-		const config = {
-			canvas,
-			type: Phaser.CANVAS,
-			autoFocus: true,
-			width: 800,
-			height: 600,
-			scale: {
-				mode: Phaser.Scale.FIT,
-				autoCenter: Phaser.Scale.CENTER_BOTH,
-				parent: "game-container",
-				autoResize: true
-			},
-			physics: {
-				default: "arcade",
-				arcade: {
-					gravity: { y: 0 },
-					fps: 60,
-					debug: false
-				}
-			},
-			scene: [BreakoutScene]
-		};
+	let currentAnswer: number | null = null;
+	let currentBid: number = 20;
 
-		new Phaser.Game(config);
-	});
+	function selectAnswer(answer: number): void {
+		currentAnswer = answer;
+		fase = FaseEnum.BID;
+	}
+
+	function dropAnswer(): void {
+		fase = FaseEnum.QUESTION;
+	}
+
+	function increaseTheBid(): void {
+		currentBid +=20;
+	}
+
+	function decreaseTheBid(): void {
+		currentBid -=20;
+	}
+
+	function startGame(): void {
+		fase = FaseEnum.QUESTION;
+	}
+
+	function restartGame(): void {
+		fase = FaseEnum.BEGIN;
+	}
 </script>
 
 <div id="game-container" class="container">
-	<canvas
-		bind:this={canvas}
-		width={800}
-		height={640}
-	>
-		Your browser does not support the HTML5 canvas tag.
-	</canvas>
+	{#if ![FaseEnum.BEGIN, FaseEnum.END].includes(fase)}
+		<h1>Раунд {round}</h1>
+		<h3>Деньги: {money}</h3>
+	{/if}
+	{#if fase === FaseEnum.BEGIN}
+		<div>
+			"Оооох...а что вчера было?...Как же болит голова...О, привет! Похоже мы отлично отметили профессиональный праздник и находимся черт знает где...Подожди...это что... ДАГЕСТАН?! Надеюсь, мы хотя бы не потратили все до последней фишки в этом чертовом казино! Нашел! ЧТО?! Всего 20$?! И как мы планируем выбираться отсюда без денег на билеты? Не знаю как ты, а вижу только один выход...придется вернуться и забрать своё!"
+			<button on:click={startGame}>Начать</button>
+		</div>
+	{/if}
+	{#if fase === FaseEnum.QUESTION}
+		<div>Выберите вариант ответа, где все методы есть методами словаря dict().</div>
+		<div>
+			<button on:click={() => selectAnswer(0)}>clear(), get(), push(), index()</button><br/>
+			<button on:click={() => selectAnswer(1)}>update(), remove(), values(), copy()</button><br/>
+			<button on:click={() => selectAnswer(2)}>setdefault(), popitem(), pop(), fromkeys()</button><br/>
+			<button on:click={() => selectAnswer(3)}>get(), set(), keys(), items()</button>
+		</div>
+	{/if}
+	{#if fase === FaseEnum.BID}
+		<div>Делайте ваши ставки</div>
+		<div>
+			<button on:click={dropAnswer}>Назад</button>
+			<button on:click={decreaseTheBid}>Опустить ставку</button>
+			<span>Текущая ставка: {currentBid}</span>
+			<button on:click={increaseTheBid}>Поднять ставку</button>
+		</div>
+	{/if}
+	{#if fase === FaseEnum.END}
+		<h1>Вы проиграли</h1>
+		<button on:click={restartGame}>Начать сначала</button>
+	{/if}
+
 </div>
 
 <style lang="sass">
-	canvas
-		background-color: #fff
-		border: 1px solid #ddd
+
 </style>
